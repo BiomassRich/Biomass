afaca6d7a67a2964d1967a0492ee9710266f609d
Richard <richardbonner837@hotmail.com>
2020-07-03 10:06:39 +0100

    Added upload to media folder from permit page

--- Biomass/views.py
+++ Biomass/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.core.files.storage import FileSystemStorage
+from datetime import datetime
 
 
 @login_required(login_url="/accounts/login/")
@@ -9,6 +11,16 @@ def homepage(request):
 
 @login_required(login_url="/accounts/login/")
 def permit(request):
+    if request.method == 'POST':
+        f = request.FILES['sentFile']
+        fs = FileSystemStorage()
+        cDate = datetime.now().strftime("%d%m%Y%H%M%S")
+        fName = f.name.replace(".pdf","") + cDate +".pdf"
+        filename = fs.save(fName, f)
+        #uploaded_file_url = fs.url(filename)
+        #print(uploaded_file_url)
+    else:
+        pass
     return render(request,'permit.html')
 
 @login_required(login_url="/accounts/login/")
